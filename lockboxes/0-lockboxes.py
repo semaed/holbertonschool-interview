b3b3094ef4eac06b602f9ca4d61f2d360265d883
semaed <figsaned@gmail.com>
2024-01-03 12:11:22 -0400

    Lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list): List of lists. Each index of the list contains a list
+        with the keys to open the next box.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if boxes is None or len(boxes) == 0:
+        return False
+
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+
+    if len(keys) == len(boxes):
+        return True
+    return False
