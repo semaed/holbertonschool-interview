bd526abe4a9181dd7799312177a4d8a47ac6316b
semaed <figsaned@gmail.com>
2024-01-23 12:25:39 -0400

    Change comments

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -19,15 +19,32 @@ def minOperations(n):
         the minimum number of operations needed to result in n characters
         or 0 if n is impossible to achieve
     """
-    if n < 2:
+    # minOperations will be the sum of all prime factors of n
+    # Check if given a valid positive int greater than 1
+    if type(n) is not int or n <= 1:
         return 0
-    count = 1
-    li = list()
-    value = n
-    while value != 1:
-        count += 1
-        if value % count == 0:
-            while value % count == 0 and value != 1:
-                value /= count
-                li.append(count)
-    return sum(li)
+    # Start summation list to store divisors
+    summation = []
+    # start divisors at 2, first prime number
+    divisor = 2
+    # divide until no longer divisible or dividing by itself
+    while (n % divisor) is 0 and (n // divisor) is not 1:
+        # each time dividing, add divisor to list to sum later
+        summation.append(divisor)
+        # update the number to show the division
+        n = n // divisor
+    # update divisor to 3, next prime number
+    divisor = 3
+    # keep going until number is less than divisor
+    while n > divisor:
+        # keep completing divisions with new odd numbers
+        while (n % divisor) is 0 and (n // divisor) is not 1:
+            summation.append(divisor)
+            n = n // divisor
+        # move divisor to next odd number
+        # not-strictly prime, but later odd numbers are comprised of previous
+        divisor += 2
+    # add the number itself, last prime number
+    summation.append(n)
+    # return the sum of all prime factors
+    return sum(summation)
