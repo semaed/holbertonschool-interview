958cdbe09487a8549a277f18a8aebaa5de73fd3a
semaed <figsaned@gmail.com>
2024-04-23 11:35:34 -0400

    correction

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,28 +1,19 @@
 #!/usr/bin/node
-// script that prints all characters of a Star Wars movie in order
+
 const request = require('request');
-const myArgs = process.argv.splice(2);
-const URL = 'https://swapi-api.hbtn.io/api/people/1/' + myArgs[0];
-request.get(URL, async (err, response, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const character = JSON.parse(body).characters;
-    const characterList = characterURLs => {
-      const promise = new Promise((resolve, reject) => {
-        request.get(characterURLs, (err, response, body) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(body);
-          }
-        });
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(filmURL + filmNum, async function (err, res, body) {
+  if (err) return console.error(err);
+  const charURLList = JSON.parse(body).characters;
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) return console.error(err);
+        console.log(JSON.parse(body).name);
+        resolve();
       });
-      return promise;
-    };
-    for (let i = 0; i < character.length; i++) {
-      const result = await characterList(character[i]);
-      console.log(JSON.parse(result).name);
-    }
+    });
   }
 });
