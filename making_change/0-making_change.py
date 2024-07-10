4751a3eda497d14f2a5f0a461ee4fd96feb9d76d
semaed <figsaned@gmail.com>
2024-07-10 09:44:38 -0400

    Correction

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
-
 """
-Change comes from within
+Defines function that determines the fewest number of coins to make change
 """
 
 
 def makeChange(coins, total):
     """
-    Return the fewest number of coins needed to meet total
+    Determines the fewest number of coins needed to meet a given total
+
+    parameters:
+        coins [list or positive ints]:
+            the values of the coins in your possession
+            you can assume you have an infinite number of coins of all values
+        total [int]:
+            total amount of change to make
+            if total is 0 or less, return 0
+
+    returns:
+        the fewest number of coins to make the change
+        or -1 if the total change cannot be made with the given coins
     """
-    temp_value = 0
-    coins.sort(reverse=True)
-
-    if total < 0:
+    if total <= 0:
         return 0
-
-    for coin in coins:
-        if total % coin <= total:
-            temp_value += total // coin
-            total = total % coin
-
-    return temp_value if total == 0 else -1
+    if len(coins) is 0:
+        return -1
+    coins = sorted(coins)
+    dynamic = [float('inf')] * (total + 1)
+    dynamic[0] = 0
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                break
+            if dynamic[i - coin] != -1:
+                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
+    if dynamic[total] == float('inf'):
+        return -1
+    return dynamic[total]
