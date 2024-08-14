3ef01ccfdd0fc8cb4395a112def13d8473e90fa2
semaed <figsaned@gmail.com>
2024-08-14 08:38:13 -0400

    Wild Compare

---
+++ wild_cmp/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* function */
+int wildcmp(char *s1, char *s2);
+
+#endif /* _HOLBERTON_H_ */
