4389afdea6f793a85a8f02bf23366418c72afc81
semaed <figsaned@gmail.com>
2024-08-18 21:09:09 -0400

    Correction

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Defines function that returns a list of lists of integers
+"""Defines function that returns a list of lists of integers
 representing the Pascal's triangle of n
 """
 
